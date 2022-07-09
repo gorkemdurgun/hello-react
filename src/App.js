385b82491ae23032791da2cd0e14b9d9a73dfab8
gorkemdurgun <gorkemdurgunn@gmail.com>
2022-07-09 18:50:53 +0300

    components ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/Header';
 
+const name = 'Görkem';
+const surname = 'Durgun';
+const isLogged = false;
+
 function App() {
   return (
-    <div >
-      <Header />
-      <Header />
-      <Header />
+    <div>
+
+      <h2>
+        {
+          isLogged 
+          ? `Adım ${name}, soyadım ${surname}` 
+          : 'Giriş Yapın'
+        }
+      </h2>
+
     </div>
   );
 }
